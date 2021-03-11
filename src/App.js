6e448c191b608472d7237efb4845daf7a3af61de
Iftekhar Rahman <iftekharmail1@gmail.com>
2021-03-11 22:13:19 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import { Button } from '@material-ui/core';
+import { useEffect, useState } from 'react';
 import './App.css';
+import News from './components/News/News';
+import axios from 'axios';
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  // useEffect( () =>{
+  //   const url = 'http://newsapi.org/v2/everything?q=tesla&from=2021-02-11&sortBy=publishedAt&apiKey=d85d2df1fd544de78339af42a7ef6885';
+  //   fetch(url)
+  //   .then(res => res.json())
+  //   .then(data => setArticles(data.articles))
+  // }, [])
+
+  useEffect( () => {
+    const url = 'http://newsapi.org/v2/everything?q=tesla&from=2021-02-11&sortBy=publishedAt&apiKey=d85d2df1fd544de78339af42a7ef6885';
+    axios(url)
+    .then(data => setArticles(data.data.articles))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Headlines: {articles.length}</h2>
+      {
+        articles.map(article => <News article={article}></News>)
+      }
     </div>
   );
 }
